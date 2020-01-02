4881caf5315cab8b6bfb96257f3fa523744ce0fa
Dmitrii_56 <ilin.dmitrii1984@gmail.com>
2020-01-02 17:08:26 +0300

    Версия 2.0

--- Generator_yield.py
+++ Generator_yield.py
@@ -4,9 +4,9 @@ def long_process(id, n):
     sum = 0
     for x in range(n):
         sum += x
-        print(id, sum, x)
+        print(id, sum, x, n)
         if x < n-1:
-            print(sum)
+            #print(sum)
             yield
         else:
             yield sum
@@ -16,30 +16,24 @@ def long_process(id, n):
 def a_lot_of_long_processes(q_ty_of_process):
     #Создается кол-во процессов long_process равное q_ty_of_process
     n = 0
-    q_ty = 0
     input_parametr=[]
-    massive_for_processes={}
     R={}
-    while q_ty <= q_ty_of_process :
+    for _ in range(q_ty_of_process):
         input_parametr.append(random.randint(q_ty_of_process,q_ty_of_process+10))
         full_process_name = "ID" +"_" + str(n)
-        massive_for_processes[full_process_name] = None
         R[full_process_name] = long_process(full_process_name,input_parametr[n])
         for key in R:
             print(R.keys())
             print(R[key])
-            if massive_for_processes[key] is None: massive_for_processes[key] = next(R[key])
-            else: print("this is Done")
         n += 1
-        q_ty += 1
-    if q_ty == q_ty_of_process+1:
-        max_input = max(input_parametr)
-        for i in range(max_input - q_ty_of_process):
-            for key in R:
-                if massive_for_processes[key] is None: massive_for_processes[key] = next(R[key])
-                else: print("this is Done" + " " + str(key))
-    print(massive_for_processes)
-
+    max_input=max(input_parametr)
+    for _ in range(max_input):
+        for key in R:
+            try :
+                next(R[key])
+            except:
+                pass
+    print(R.keys(), R.values())
 
 a_lot_of_long_processes(7)
 
