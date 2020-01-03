223ecb77835df72c0d11997d7146b7f23ba7f3ee
Dmitrii_56 <ilin.dmitrii1984@gmail.com>
2020-01-03 22:16:23 +0300

    Реализация генератора v 3.0

--- Generator_yield.py
+++ Generator_yield.py
@@ -15,24 +15,25 @@ def long_process(id, n):
 
 def a_lot_of_long_processes(q_ty_of_process):
     #Создается кол-во процессов long_process равное q_ty_of_process
-    n = 0
-    input_parametr=[]
+    n = 0  
     R={}
     for _ in range(q_ty_of_process):
-        input_parametr.append(random.randint(q_ty_of_process,q_ty_of_process+10))
         full_process_name = "ID" +"_" + str(n)
-        R[full_process_name] = long_process(full_process_name,input_parametr[n])
+        R[full_process_name] = long_process(full_process_name,random.randint(q_ty_of_process,q_ty_of_process+10))
         for key in R:
             print(R.keys())
             print(R[key])
         n += 1
-    max_input=max(input_parametr)
-    for _ in range(max_input):
+    while True == True :
+        flag = True
         for key in R:
             try :
                 next(R[key])
+                flag = False 
             except:
                 pass
+        if flag == True:
+            break
     print(R.keys(), R.values())
 
 a_lot_of_long_processes(7)
