10d287babaf585c70a53279c3f6742b2acb46758
Dmitrii_56 <ilin.dmitrii1984@gmail.com>
2020-01-04 20:53:39 +0300

    Рефакторинг кода генератора

--- Generator_yield.py
+++ Generator_yield.py
@@ -1,6 +1,7 @@
 import random
 
 def long_process(id, n):
+    #Ф-ция суммирования n чисел. Возвращает сумму, промежуточный результат суммы равен None-то что вернет данная функция
     sum = 0
     for x in range(n):
         sum += x
@@ -10,6 +11,22 @@ def long_process(id, n):
             yield
         else:
             yield sum
+
+
+def create_input_data(n):
+    #Ф-ция создает массив с n случайных чисел
+    output=[]
+    for i in range(n):
+        output.append(random.randint(1,10))
+    return output
+
+def create_generators(input_data):
+    #Функция получает на вход массив состоящий из данных типа int, запускает N процессов long_process, где N кол-во данных в input_data
+    R={}
+    for i in range(len(input_data)):
+        full_process_name = "ID" +"_" + str(i)
+        R[full_process_name] = long_process(full_process_name, i)
+    return R
 
 
 
@@ -36,6 +53,5 @@ def a_lot_of_long_processes(q_ty_of_process):
             break
     print(R.keys(), R.values())
 
-a_lot_of_long_processes(7)
 
 
